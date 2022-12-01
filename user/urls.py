4d569256ac690f5350864523f37afd60785f221f
Vlad <vladh17dmv@mail.ru>
2022-12-01 23:21:35 +0300

    homepage

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 
+from user.views import *
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', HomePage.as_view(), name='home'),
 ]
